cee50cd77b00795f3ba3fe8e0f7f0a2fd7c589e6
doob187 <60312740+doob187@users.noreply.github.com>
2023-01-15 15:13:58 +0100

    Update index.js

--- material-dashboard-react/src/examples/Tables/DataTable/index.js
+++ material-dashboard-react/src/examples/Tables/DataTable/index.js
@@ -13,7 +13,7 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
-/* eslint react/prop-types: 0 */
+/* eslint react/forbid-prop-types: 0 */
 
 import { useMemo, useEffect, useState } from "react";
 
