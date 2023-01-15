9c0e8b02872269ed29035b97ce49b96871d8829c
doob187 <60312740+doob187@users.noreply.github.com>
2023-01-15 15:14:55 +0100

    Update index.js

--- material-dashboard-react/src/examples/Sidenav/index.js
+++ material-dashboard-react/src/examples/Sidenav/index.js
@@ -12,6 +12,7 @@ Coded by www.creative-tim.com
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
+/* eslint react/forbid-prop-types: 0 */
 
 import { useEffect } from "react";
 
