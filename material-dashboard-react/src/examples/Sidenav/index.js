e9714b1d905a5051298b0f73e5cbbd2d29624f43
doob187 <60312740+doob187@users.noreply.github.com>
2023-01-15 14:35:21 +0100

    Update index.js

--- material-dashboard-react/src/examples/Sidenav/index.js
+++ material-dashboard-react/src/examples/Sidenav/index.js
@@ -30,7 +30,7 @@ import Icon from "@mui/material/Icon";
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
-import MDButton from "components/MDButton";
+// import MDButton from "components/MDButton";
 
 // Material Dashboard 2 React example components
 import SidenavCollapse from "examples/Sidenav/SidenavCollapse";
