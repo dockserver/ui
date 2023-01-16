babfc17c28b7bbaf62f01c81494d740e2fe1b15a
doob187 <60312740+doob187@users.noreply.github.com>
2023-01-16 22:30:45 +0100

    Update DefaultNavbarMobile.js

--- material-dashboard-react/src/examples/Navbars/DefaultNavbar/DefaultNavbarMobile.js
+++ material-dashboard-react/src/examples/Navbars/DefaultNavbar/DefaultNavbarMobile.js
@@ -45,10 +45,10 @@ function DefaultNavbarMobile({ open, close }) {
       MenuListProps={{ style: { width: `calc(${width}px - 4rem)` } }}
     >
       <MDBox px={0.5}>
-        <DefaultNavbarLink icon="donut_large" name="dashboard" route="/dashboard" />
+        <DefaultNavbarLink icon="donut_large" name="dashboard" route="/" />
         <DefaultNavbarLink icon="person" name="profile" route="/profile" />
-        <DefaultNavbarLink icon="account_circle" name="sign up" route="/authentication/sign-up" />
-        <DefaultNavbarLink icon="key" name="sign in" route="/authentication/sign-in" />
+        <DefaultNavbarLink icon="account_circle" name="inprogress" route="/inprogress" />
+        <DefaultNavbarLink icon="key" name="completed" route="/completed" />
       </MDBox>
     </Menu>
   );
