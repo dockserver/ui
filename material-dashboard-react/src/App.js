3efc077334b9c394ca556ea84679e04e978e1270
doob187 <60312740+doob187@users.noreply.github.com>
2023-01-21 21:32:51 +0100

    Update App.js

--- material-dashboard-react/src/App.js
+++ material-dashboard-react/src/App.js
@@ -1,6 +1,6 @@
 /**
 =========================================================
-* Uploader Dashboard 2 - v2.1.0
+* Material Dashboard 2 React - v2.1.0
 =========================================================
 
 * Product Page: https://www.creative-tim.com/product/material-dashboard-react
@@ -23,15 +23,18 @@ import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import Icon from "@mui/material/Icon";
 
-// Uploader Dashboard 2 components
+// Material Dashboard 2 React components
 import MDBox from "components/MDBox";
 
-// Uploader Dashboard 2 example components
+// Material Dashboard 2 React example components
 import Sidenav from "examples/Sidenav";
 import Configurator from "examples/Configurator";
 
+// Material Dashboard 2 React themes
+import theme from "assets/theme";
+import themeRTL from "assets/theme/theme-rtl";
 
-// Uploader Dashboard 2 Dark Mode themes
+// Material Dashboard 2 React Dark Mode themes
 import themeDark from "assets/theme-dark";
 import themeDarkRTL from "assets/theme-dark/theme-rtl";
 
@@ -40,10 +43,10 @@ import rtlPlugin from "stylis-plugin-rtl";
 import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
 
-// Uploader Dashboard 2 routes
+// Material Dashboard 2 React routes
 import routes from "routes";
 
-// Uploader Dashboard 2 contexts
+// Material Dashboard 2 React contexts
 import { useMaterialUIController, setMiniSidenav, setOpenConfigurator } from "context";
 
 // Images
@@ -51,157 +54,145 @@ import brandWhite from "assets/images/logo-ct.png";
 import brandDark from "assets/images/logo-ct-dark.png";
 
 export default function App() {
-    const [controller, dispatch] = useMaterialUIController();
-    const {
-        miniSidenav,
-        direction,
-        layout,
-        openConfigurator,
-        sidenavColor,
-        transparentSidenav,
-        whiteSidenav,
-        darkMode,
-    } = controller;
-    const [onMouseEnter, setOnMouseEnter] = useState(false);
-    const [rtlCache, setRtlCache] = useState(null);
-    const { pathname } = useLocation();
+  const [controller, dispatch] = useMaterialUIController();
+  const {
+    miniSidenav,
+    direction,
+    layout,
+    openConfigurator,
+    sidenavColor,
+    transparentSidenav,
+    whiteSidenav,
+    darkMode,
+  } = controller;
+  const [onMouseEnter, setOnMouseEnter] = useState(false);
+  const [rtlCache, setRtlCache] = useState(null);
+  const { pathname } = useLocation();
 
-    // Cache for the rtl
-    useMemo(() => {
-        const cacheRtl = createCache({
-            key: "rtl",
-            stylisPlugins: [rtlPlugin],
-        });
+  // Cache for the rtl
+  useMemo(() => {
+    const cacheRtl = createCache({
+      key: "rtl",
+      stylisPlugins: [rtlPlugin],
+    });
 
-        setRtlCache(cacheRtl);
-    }, []);
+    setRtlCache(cacheRtl);
+  }, []);
 
-    // Open sidenav when mouse enter on mini sidenav
-    const handleOnMouseEnter = () => {
-        if (miniSidenav && !onMouseEnter) {
-            setMiniSidenav(dispatch, false);
-            setOnMouseEnter(true);
-        }
-    };
+  // Open sidenav when mouse enter on mini sidenav
+  const handleOnMouseEnter = () => {
+    if (miniSidenav && !onMouseEnter) {
+      setMiniSidenav(dispatch, false);
+      setOnMouseEnter(true);
+    }
+  };
 
-    // Close sidenav when mouse leave mini sidenav
-    const handleOnMouseLeave = () => {
-        if (onMouseEnter) {
-            setMiniSidenav(dispatch, true);
-            setOnMouseEnter(false);
-        }
-    };
+  // Close sidenav when mouse leave mini sidenav
+  const handleOnMouseLeave = () => {
+    if (onMouseEnter) {
+      setMiniSidenav(dispatch, true);
+      setOnMouseEnter(false);
+    }
+  };
 
-    // Change the openConfigurator state
-    const handleConfiguratorOpen = () => setOpenConfigurator(dispatch, !openConfigurator);
+  // Change the openConfigurator state
+  const handleConfiguratorOpen = () => setOpenConfigurator(dispatch, !openConfigurator);
 
-    // Setting the dir attribute for the body element
-    useEffect(() => {
-        document.body.setAttribute("dir", direction);
-    }, [direction]);
+  // Setting the dir attribute for the body element
+  useEffect(() => {
+    document.body.setAttribute("dir", direction);
+  }, [direction]);
 
-    // Setting page scroll to 0 when changing the route
-    useEffect(() => {
-        document.documentElement.scrollTop = 0;
-        document.scrollingElement.scrollTop = 0;
-    }, [pathname]);
+  // Setting page scroll to 0 when changing the route
+  useEffect(() => {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+  }, [pathname]);
 
-    const getRoutes = (allRoutes) =>
-        allRoutes.map((route) => {
-            if (route.collapse) {
-                return getRoutes(route.collapse);
-            }
+  const getRoutes = (allRoutes) =>
+    allRoutes.map((route) => {
+      if (route.collapse) {
+        return getRoutes(route.collapse);
+      }
 
-            if (route.route) {
-                return <Route exact path = { route.route }
-                element = { route.component }
-                key = { route.key }
-                />;
-            }
+      if (route.route) {
+        return <Route exact path={route.route} element={route.component} key={route.key} />;
+      }
 
-            return null;
-        });
+      return null;
+    });
 
-    const configsButton = ( <
-        MDBox display = "flex"
-        justifyContent = "center"
-        alignItems = "center"
-        width = "3.25rem"
-        height = "3.25rem"
-        bgColor = "white"
-        shadow = "sm"
-        borderRadius = "50%"
-        position = "fixed"
-        right = "2rem"
-        bottom = "2rem"
-        zIndex = { 99 }
-        color = "dark"
-        sx = {
-            { cursor: "pointer" }
-        }
-        onClick = { handleConfiguratorOpen } >
-        <
-        Icon fontSize = "small"
-        color = "inherit" >
-        settings <
-        /Icon> < /
-        MDBox >
-    );
+  const configsButton = (
+    <MDBox
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      width="3.25rem"
+      height="3.25rem"
+      bgColor="white"
+      shadow="sm"
+      borderRadius="50%"
+      position="fixed"
+      right="2rem"
+      bottom="2rem"
+      zIndex={99}
+      color="dark"
+      sx={{ cursor: "pointer" }}
+      onClick={handleConfiguratorOpen}
+    >
+      <Icon fontSize="small" color="inherit">
+        settings
+      </Icon>
+    </MDBox>
+  );
 
-    return direction === "rtl" ? ( <
-        CacheProvider value = { rtlCache } >
-        <
-        ThemeProvider theme = { darkMode ? themeDarkRTL : themeRTL } >
-        <
-        CssBaseline / > {
-            layout === "dashboard" && ( <
-                >
-                <
-                Sidenav color = { sidenavColor }
-                brand = {
-                    (transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite
-                }
-                brandName = "Uploader Dashboard 2"
-                routes = { routes }
-                onMouseEnter = { handleOnMouseEnter }
-                onMouseLeave = { handleOnMouseLeave }
-                /> <
-                Configurator / > { configsButton } <
-                />
-            )
-        } { layout === "vr" && < Configurator / > } <
-        Routes > { getRoutes(routes) } <
-        Route path = "*"
-        element = { < Navigate to = "/" / > }
-        /> < /
-        Routes > <
-        /ThemeProvider> < /
-        CacheProvider >
-    ) : ( <
-        ThemeProvider theme = { darkMode ? themeDark : theme } >
-        <
-        CssBaseline / > {
-            layout === "dashboard" && ( <
-                >
-                <
-                Sidenav color = { sidenavColor }
-                brand = {
-                    (transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite
-                }
-                brandName = "Material Dashboard 2"
-                routes = { routes }
-                onMouseEnter = { handleOnMouseEnter }
-                onMouseLeave = { handleOnMouseLeave }
-                /> <
-                Configurator / > { configsButton } <
-                />
-            )
-        } { layout === "vr" && < Configurator / > } <
-        Routes > { getRoutes(routes) } <
-        Route path = "*"
-        element = { < Navigate to = "/" / > }
-        /> < /
-        Routes > <
-        /ThemeProvider>
-    );
+  return direction === "rtl" ? (
+    <CacheProvider value={rtlCache}>
+      <ThemeProvider theme={darkMode ? themeDarkRTL : themeRTL}>
+        <CssBaseline />
+        {layout === "dashboard" && (
+          <>
+            <Sidenav
+              color={sidenavColor}
+              brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
+              brandName="Uplaoder Dashboard 2"
+              routes={routes}
+              onMouseEnter={handleOnMouseEnter}
+              onMouseLeave={handleOnMouseLeave}
+            />
+            <Configurator />
+            {configsButton}
+          </>
+        )}
+        {layout === "vr" && <Configurator />}
+        <Routes>
+          {getRoutes(routes)}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </ThemeProvider>
+    </CacheProvider>
+  ) : (
+    <ThemeProvider theme={darkMode ? themeDark : theme}>
+      <CssBaseline />
+      {layout === "dashboard" && (
+        <>
+          <Sidenav
+            color={sidenavColor}
+            brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
+            brandName="Uploader Dashboard 2"
+            routes={routes}
+            onMouseEnter={handleOnMouseEnter}
+            onMouseLeave={handleOnMouseLeave}
+          />
+          <Configurator />
+          {configsButton}
+        </>
+      )}
+      {layout === "vr" && <Configurator />}
+      <Routes>
+        {getRoutes(routes)}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </ThemeProvider>
+  );
 }
