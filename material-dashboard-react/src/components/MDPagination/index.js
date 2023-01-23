1cbcf2a1bdae55e7c53e7eb10969d1bdba93cb66
doob187 <60312740+doob187@users.noreply.github.com>
2023-01-23 12:11:50 +0100

    Update index.js

--- material-dashboard-react/src/components/MDPagination/index.js
+++ material-dashboard-react/src/components/MDPagination/index.js
@@ -1,6 +1,6 @@
 /**
 =========================================================
-* Uploader Dashboard 2 - v2.1.0
+* Material Dashboard 2 React - v2.1.0
 =========================================================
 
 * Product Page: https://www.creative-tim.com/product/material-dashboard-react
@@ -18,7 +18,7 @@ import { forwardRef, createContext, useContext, useMemo } from "react";
 // prop-types is a library for typechecking of props
 import PropTypes from "prop-types";
 
-// Uploader Dashboard 2 components
+// Material Dashboard 2 React components
 import MDBox from "components/MDBox";
 
 // Custom styles for MDPagination
@@ -28,65 +28,68 @@ import MDPaginationItemRoot from "components/MDPagination/MDPaginationItemRoot";
 const Context = createContext();
 
 const MDPagination = forwardRef(
-    ({ item, variant, color, size, active, children, ...rest }, ref) => {
-        const context = useContext(Context);
-        const paginationSize = context ? context.size : null;
+  ({ item, variant, color, size, active, children, ...rest }, ref) => {
+    const context = useContext(Context);
+    const paginationSize = context ? context.size : null;
 
-        const value = useMemo(() => ({ variant, color, size }), [variant, color, size]);
+    const value = useMemo(() => ({ variant, color, size }), [variant, color, size]);
 
-        return ( <
-            Context.Provider value = {value} > {
-                item ? ( <
-                    MDPaginationItemRoot {...rest}
-                    ref = {ref}
-                    variant = {active ? context.variant : "outlined"}
-                    color = {active ? context.color : "secondary"}
-                    iconOnly circular ownerState = {
-                        { variant, active, paginationSize }
-                    } > { children } <
-                    /MDPaginationItemRoot>
-                ) : ( <
-                    MDBox display = "flex"
-                    justifyContent = "flex-end"
-                    alignItems = "center"
-                    sx = {
-                        { listStyle: "none" }
-                    } > { children } <
-                    /MDBox>
-                )
-            } <
-            /Context.Provider>
-        );
-    }
+    return (
+      <Context.Provider value={value}>
+        {item ? (
+          <MDPaginationItemRoot
+            {...rest}
+            ref={ref}
+            variant={active ? context.variant : "outlined"}
+            color={active ? context.color : "secondary"}
+            iconOnly
+            circular
+            ownerState={{ variant, active, paginationSize }}
+          >
+            {children}
+          </MDPaginationItemRoot>
+        ) : (
+          <MDBox
+            display="flex"
+            justifyContent="flex-end"
+            alignItems="center"
+            sx={{ listStyle: "none" }}
+          >
+            {children}
+          </MDBox>
+        )}
+      </Context.Provider>
+    );
+  }
 );
 
 // Setting default values for the props of MDPagination
 MDPagination.defaultProps = {
-    item: false,
-    variant: "gradient",
-    color: "info",
-    size: "medium",
-    active: false,
+  item: false,
+  variant: "gradient",
+  color: "info",
+  size: "medium",
+  active: false,
 };
 
 // Typechecking props for the MDPagination
 MDPagination.propTypes = {
-    item: PropTypes.bool,
-    variant: PropTypes.oneOf(["gradient", "contained"]),
-    color: PropTypes.oneOf([
-        "white",
-        "primary",
-        "secondary",
-        "info",
-        "success",
-        "warning",
-        "error",
-        "light",
-        "dark",
-    ]),
-    size: PropTypes.oneOf(["small", "medium", "large"]),
-    active: PropTypes.bool,
-    children: PropTypes.node.isRequired,
+  item: PropTypes.bool,
+  variant: PropTypes.oneOf(["gradient", "contained"]),
+  color: PropTypes.oneOf([
+    "white",
+    "primary",
+    "secondary",
+    "info",
+    "success",
+    "warning",
+    "error",
+    "light",
+    "dark",
+  ]),
+  size: PropTypes.oneOf(["small", "medium", "large"]),
+  active: PropTypes.bool,
+  children: PropTypes.node.isRequired,
 };
 
 export default MDPagination;
